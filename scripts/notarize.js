37cc97d4803c862ca308c0af0bc9d1d4451fa2c6
Nicolás López <nicolaslopezj@me.com>
2023-03-24 16:01:15 -0300

    init

---
+++ scripts/notarize.js
@@ -0,0 +1,20 @@
+const { notarize } = require('electron-notarize');
+require('dotenv').config();
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  // Package your app here, and code sign with hardened runtime
+  await notarize({
+    tool: 'notarytool',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+};
