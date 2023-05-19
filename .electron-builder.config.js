e212cb573839ce6abd98f10eacb2541c73bfbd23
Nicolás López <nicolaslopezj@me.com>
2023-05-19 13:12:10 -0400

    corrije el electron builder

---
+++ .electron-builder.config.js
@@ -0,0 +1,54 @@
+/**
+ * @type {() => import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configurtagation
+ */
+module.exports = async function () {
+  return {
+    productName: 'Crisp',
+    appId: 'com.getjusto.crisp',
+    afterSign: 'electron-builder-notarize',
+    directories: {
+      output: 'dist',
+      buildResources: 'build'
+    },
+    publish: {
+      provider: 'github',
+      publishAutoUpdate: true
+    },
+    mac: {
+      hardenedRuntime: true,
+      electronLanguages: ['en'],
+      icon: 'build/icon.icns',
+      entitlements: './node_modules/electron-builder-notarize/entitlements.mac.inherit.plist',
+      publish: ['github'],
+      target: [
+        {
+          target: 'dmg',
+          arch: ['arm64', 'x64']
+        },
+        {
+          target: 'zip',
+          arch: ['arm64', 'x64']
+        }
+      ]
+    },
+    win: {
+      publish: ['github'],
+      icon: 'build/icon.ico',
+      target: [
+        {
+          target: 'nsis',
+          arch: ['x64', 'ia32']
+        }
+      ]
+    },
+    nsis: {
+      oneClick: true,
+      perMachine: true
+    },
+    dmg: {
+      icon: 'build/icon.icns',
+      internetEnabled: true
+    }
+  }
+}
