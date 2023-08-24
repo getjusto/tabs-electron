5777f35de3d65603997f2f80c73db35400c0647f
Nicolás López <nicolaslopezj@me.com>
2023-08-24 16:30:33 -0400

    upgrades electron deps

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,21 +1,19 @@
-const { notarize } = require('electron-notarize');
-require('dotenv').config();
+import {notarize} from '@electron/notarize'
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
+  const {electronPlatformName, appOutDir} = context
   if (electronPlatformName !== 'darwin') {
-    return;
+    return
   }
 
-  const appName = context.packager.appInfo.productFilename;
+  const appName = context.packager.appInfo.productFilename
 
   // Package your app here, and code sign with hardened runtime
   await notarize({
     appBundleId: 'com.getjusto.crisp',
-    tool: 'notarytool',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_PASSWORD,
-    teamId: process.env.APPLE_TEAM_ID,
-  });
-};
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID
+  })
+}
